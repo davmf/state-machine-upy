491bc7497391e77b9890a10809f2524d54f9b485
David Fyfe <david@davidfyfe.net>
2022-03-21 22:41:15 +1100

    WIP

--- src/main.py
+++ src/main.py
@@ -2,59 +2,52 @@ import asyncio
 from typing import *
 from events import Events
 from state import State
-from state_machine import StateMachine
-from state_machines import StateMachineA, StateMachineB
+from states import MainInitial, MainA, MainB
 
 
 class Main(State):
 
     def __init__(self) -> None:
         super().__init__()
-        self.state_machine_A = StateMachineA()
-        self.state_machine_B = StateMachineB()
-        self.state = State()
-
-    def enter(self):
-        pass
+        self.state_A = MainA()
+        self.state_B = MainB()
+        self.state = MainInitial()
 
     async def do(self) -> None:
         self.log.info("")
-        await asyncio.sleep(4)
-        Events.set_(Events.EV2)
-        await asyncio.sleep(4)
-        Events.set_(Events.EV2)
-        await asyncio.sleep(4)
-        Events.set_(Events.EV4)
-        await asyncio.sleep(4)
-        Events.set_(Events.EV4)
+        asyncio.create_task(self.manage())
+
+        while True:
+            await asyncio.sleep(2)
+            Events.set_(Events.EV2)
+            await asyncio.sleep(2)
+            Events.set_(Events.EV1)
 
         while True:
             await asyncio.sleep(1)
 
     async def manage(self):
         self.log.info("")
-        self.state.transition_to(self.state_machine_A)
+        self.state = await self.state.transition_to(self.state_A)
 
         while True:
             await Events.get(Events.EV1 | Events.EV2)
 
-            if self.state == self.state_machine_A:
+            if self.state == self.state_A:
                 if Events.is_set(Events.EV1):
-                    await self.state.transition_to(self.state_machine_B)
-            elif self.state == self.state_machine_B:
+                    self.state = await self.state.transition_to(self.state_B)
+            elif self.state == self.state_B:
                 if Events.is_set(Events.EV2):
-                    await self.state.transition_to(self.state_machine_A)
-    
-    def exit(self):
-        pass
+                    self.state = await self.state.transition_to(self.state_A)   
+
+
+def start(state_machine) -> asyncio.Task:
+    return asyncio.create_task(state_machine.do())
 
 
 async def main():
-    state_machine = Main()
-    asyncio.create_task(state_machine.run())
-
-    while True:
-        await asyncio.sleep(1)
+    state_machine_task = start(Main())
+    await state_machine_task
 
 
 asyncio.run(main())
--- src/states.py
+++ src/states.py
@@ -1,21 +1,103 @@
+import asyncio
 from typing import *
+
+from events import Events
+
+import logger
 from state import State
 
-import asyncio
+
+class MainA(State):
+
+    def __init__(self):
+        super().__init__()
+        self.initial = MainAInitial()
+        self.state_AA = MainAA()
+        self.state_AB = MainAB()
+        self.state = self.initial
+        self.log = logger.init_logging(type(self).__name__)
+
+    async def manage(self):
+        self.log.info("")
+        self.state = await self.state.transition_to(self.state_AA)
+
+        try:
+            while True:
+                await Events.get(Events.EV2 | Events.EV3)
+
+                if self.state == self.state_AA:
+                    if Events.is_set(Events.EV2):
+                        self.state = await self.state.transition_to(self.state_AB)
+                elif self.state == self.state_AB:
+                    if Events.is_set(Events.EV2):
+                        Events.set_(Events.EV1)
+                    elif Events.is_set(Events.EV3):
+                        self.state = await self.state.transition_to(self.state_AA)
+
+        except asyncio.CancelledError:
+            pass
 
 
+class MainB(State):
 
-class StateAA(State):
+    def __init__(self):
+        super().__init__()
+        self.initial = MainBInitial()
+        self.state_BA= MainBA()
+        self.state_BB = MainBB()
+        self.state = self.initial
+        self.log = logger.init_logging(type(self).__name__)
+
+    async def manage(self):
+        self.log.info("")
+        self.state = await self.state.transition_to(self.state_BA)
+
+        try:
+            while True:
+                await Events.get(Events.EV4 | Events.EV5)
+
+                if self.state == self.state_BA:
+                    if Events.is_set(Events.EV4):
+                        self.state = await self.state.transition_to(self.state_BB)
+                    elif Events.is_set(Events.EV5):
+                        Events.set_(Events.EV2)
+                elif self.state == self.state_BB:
+                    if Events.is_set(Events.EV4):
+                        Events.set_(Events.EV2)
+
+        except asyncio.CancelledError:
+            pass
+
+
+class MainInitial(State):
+
+    def __init__(self):
+        super().__init__()
+
+
+class MainAInitial(State):
+
+    def __init__(self):
+        super().__init__()
+
+
+class MainBInitial(State):
+
+    def __init__(self):
+        super().__init__()
+
+
+class MainAA(State):
     pass    
 
 
-class StateAB(State):
+class MainAB(State):
     pass
 
 
-class StateBA(State):
+class MainBA(State):
     pass    
 
 
-class StateBB(State):
+class MainBB(State):
     pass
